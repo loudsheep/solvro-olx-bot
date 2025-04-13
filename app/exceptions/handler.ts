da4025ede3d92f3aefe1641fdbb3b6e37b9d9621
loudsheep <loudsheep05@gmail.com>
2025-04-13 10:50:10 +0200

    fix: prettier check

--- app/exceptions/handler.ts
+++ app/exceptions/handler.ts
@@ -1,26 +1,26 @@
-import app from '@adonisjs/core/services/app'
-import { HttpContext, ExceptionHandler } from '@adonisjs/core/http'
+import { ExceptionHandler, HttpContext } from "@adonisjs/core/http";
+import app from "@adonisjs/core/services/app";
 
 export default class HttpExceptionHandler extends ExceptionHandler {
   /**
    * In debug mode, the exception handler will display verbose errors
    * with pretty printed stack traces.
    */
-  protected debug = !app.inProduction
+  protected debug = !app.inProduction;
 
   /**
    * Status pages are used to display a custom HTML pages for certain error
    * codes. You might want to enable them in production only, but feel
    * free to enable them in development as well.
    */
-  protected renderStatusPages = app.inProduction
+  protected renderStatusPages = app.inProduction;
 
   /**
    * The method is used for handling errors and returning
    * response to the client
    */
   async handle(error: unknown, ctx: HttpContext) {
-    return super.handle(error, ctx)
+    return super.handle(error, ctx);
   }
 
   /**
@@ -30,6 +30,6 @@ export default class HttpExceptionHandler extends ExceptionHandler {
    * @note You should not attempt to send a response from this method.
    */
   async report(error: unknown, ctx: HttpContext) {
-    return super.report(error, ctx)
+    return super.report(error, ctx);
   }
 }
